c53e92a0d3fc7118af2084eabc404bdeb3c55e14
jiangwei <jiangwei07061625@163.com>
2019-07-05 12:00:49 +0800

    init

--- README.md
+++ README.md
@@ -10,12 +10,11 @@
 
 ## 依赖
 ### 必选
-go get github.com/json-iterator/go //json解析
-go get github.com/axgle/mahonia //编码转换
-
+- github.com/json-iterator/go: json解析
+- github.com/axgle/mahonia: 编码转换
 ### 可选
-go get github.com/gocolly/colly //爬虫框架
-go get github.com/PuerkitoBio/goquery //爬虫选择器
+- github.com/gocolly/colly: 爬虫框架
+- github.com/PuerkitoBio/goquery: 爬虫选择器
 
 # 使用
 # 资源
--- common/utils/tool.go
+++ common/utils/tool.go
@@ -1,1 +1,14 @@
 package utils
+
+import (
+	"github.com/axgle/mahonia"
+)
+
+//字符串编码转换
+func JConvertStr(str string, oldEncoder string, newEncoder string) string {
+	srcDecoder := mahonia.NewDecoder(oldEncoder)
+	desDecoder := mahonia.NewDecoder(newEncoder)
+	resStr := srcDecoder.ConvertString(str)
+	_, resBytes, _ := desDecoder.Translate([]byte(resStr), true)
+	return string(resBytes)
+}
