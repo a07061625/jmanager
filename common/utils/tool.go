6c67083eb472bf30fcab8c74ff30f1cf85493693
jiangwei <jiangwei07061625@163.com>
2019-07-05 14:13:28 +0800

    init

--- common/utils/tool.go
+++ common/utils/tool.go
@@ -2,7 +2,10 @@ package utils
 
 import (
 	"github.com/axgle/mahonia"
+	"github.com/json-iterator/go"
 )
+
+var jsonIterator  = jsoniter.ConfigCompatibleWithStandardLibrary
 
 //字符串编码转换
 func JConvertStr(str string, oldEncoder string, newEncoder string) string {
@@ -11,4 +14,14 @@ func JConvertStr(str string, oldEncoder string, newEncoder string) string {
 	resStr := srcDecoder.ConvertString(str)
 	_, resBytes, _ := desDecoder.Translate([]byte(resStr), true)
 	return string(resBytes)
+}
+
+//JSON编码
+func JJsonEncode(obj interface{}) ([]byte, error) {
+	return jsonIterator.Marshal(obj)
+}
+
+//JSON解码
+func JJsonDecode(jsonBlob []byte, obj interface{}) error {
+	return jsonIterator.Unmarshal(jsonBlob, &obj)
 }
